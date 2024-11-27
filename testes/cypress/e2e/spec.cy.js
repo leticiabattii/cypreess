0648e1ec598f3a515fe1cecb19ddd0173f792970
leticiabattii <leticia_a_vaz@estudante.sesisenai.org.br>
2024-11-27 16:27:02 -0300

    cypreess

--- testes/cypress/e2e/spec.cy.js
+++ testes/cypress/e2e/spec.cy.js
@@ -4,13 +4,13 @@ describe('Testando o taskhub', () => {
   });
   it('Cadastro no taskhub', () => {
     cy.get('a').contains('Get Started').click()
-    cy.get('#name').type('emily')
-    cy.get('#email').type('emilycypress@gmail.com') 
-    cy.get('#password').type('emily147')
+    cy.get('#name').type('lele')
+    cy.get('#email').type('lelecypress@gmail.com') 
+    cy.get('#password').type('lele147')
     cy.get('button[type="submit"]').click()
     cy.get('a').contains('Sign In').click()
-    cy.get('#email').type('emilycypress@gmail.com') 
-    cy.get('#password').type('emily147')
+    cy.get('#email').type('lelecypress@gmail.com') 
+    cy.get('#password').type('lele147')
     cy.get('[data-cy=botao-login]').click()
     cy.get('a').contains('Projects').click()
     cy.get('button[type="button"]').contains('Create Project').click()
@@ -18,7 +18,7 @@ describe('Testando o taskhub', () => {
     cy.get('#description').type('Realizando um teste!') 
     cy.get('#owner').select('Select an owner')
     cy.get('#owner').select(1); 
-    cy.get('#react-select-2-input').click().type('emily{enter}')
+    cy.get('#react-select-2-input').click().type('lele{enter}')
     cy.get('#react-select-3-input').click().type('teste{enter}')
     cy.get('button[type="submit"]').contains('Create Project').click()
   });
